2947266480614069ce4a8399807dabf5a1bdd66b
Adeline Johnson <groblewski.adeline@gmail.com>
2018-08-12 11:45:01 -0400

    some updates

--- client/src/components/SocialCard/SocialCard.js
+++ client/src/components/SocialCard/SocialCard.js
@@ -6,6 +6,9 @@ import Signature from "../Signature";
 const SocialCard = () => (
   <div className="social-card">
     <Thumbnail/>
+
+    <div className="middle-writing"> CLICK ME FOR MORE INFO! </div> 
+
     <Signature/>
     
   </div>
